b8eccc4d48f9d2541c2a0cd883471b09b851e9ce
Steve <kimste2@gmail.com>
2026-04-01 16:07:46 -0400

    start of simple GUI

--- Qr Code/UI/ui.py
+++ Qr Code/UI/ui.py
@@ -1,2 +1,33 @@
 import FreeSimpleGUI as gui
- 
+
+# Define the window's contents
+start_layout = [  [gui.Text("URL: ")],
+            [gui.Input(key="-url-")],
+            [gui.Text("Select a file:")],
+            [gui.Input(key="-file-"), gui.FileBrowse(initial_folder="C:\\")],
+            [gui.Button('Continue', key="-continue-")]
+]
+
+processing_layout = [
+    [gui.Text("Processing, please wait...")],
+    [gui.ProgressBar(max_value=100, orientation='h', size=(20, 20), key='progress')],
+    [gui.Cancel()]
+]
+
+# Create the window
+window = gui.Window('Generate a QR Code', start_layout)
+
+while True:
+        event, values = window.read()
+
+        if event == gui.WINDOW_CLOSED or event == 'Quit':
+            break
+
+        if (event == "-continue-" and values["-url-"] != ""and
+        values["-file-"] != ""):
+            print("forward")
+            window.close()
+            window = gui.Window("Processing ...", processing_layout)
+
+
+window.close()
