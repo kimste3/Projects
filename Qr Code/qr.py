1e8ed04fd7fff74afc23f8032ea9026a5a059458
Steve <kimste2@gmail.com>
2026-04-04 15:24:41 -0400

    change signature to account for a filename

--- Qr Code/qr.py
+++ Qr Code/qr.py
@@ -1,5 +1,11 @@
 import qrcode as q
 
-def make_qr(url, filename):
+
+## url: url you want people to click on on
+## save_directory location on computer where you want to save the PNG file
+## name of QR code you want 
+
+def make_qr(url, save_directory, filename):
+    location = save_directory + "\\" + filename + ".png"
     img = q.make(url)
-    img.save(filename)
+    img.save(location)
