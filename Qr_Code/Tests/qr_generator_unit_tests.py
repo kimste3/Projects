989d470a52b8bc99df5f2a9feb20b8430bffefe8
Steve <kimste2@gmail.com>
2026-04-29 18:27:03 -0400

    add simple type test, change file format to a variable

--- Qr_Code/Tests/qr_generator_unit_tests.py
+++ Qr_Code/Tests/qr_generator_unit_tests.py
@@ -7,15 +7,19 @@ import os
 url = "https://www.google.com"
 file = r"C:\\Users\\kimst\\Desktop"
 name = "google"
-
+file_format =".png"
 q = Qr_Generator(url, file, name)
 def test_get_URL_google(url=url):
     assert url == q.get_url()
 
 
-filepath = f"{file}\\{name}.png"
+filepath = f"{file}\\{name}{file_format}"
 def test_get_file_location(filepath=filepath):
     assert filepath == q.get_location()
 
 def test_get_file_name_with_google(name=name):
     assert name == q.get_filename()
+
+def test_q_type():
+    assert type(q) == Qr_Generator
+
